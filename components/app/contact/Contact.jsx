324691c7335985a84a33a61891a624111e0d0b87
William <anazawilliam1@gmail.com>
2024-04-30 08:26:05 +0100

    contact section

--- components/app/contact/Contact.jsx
+++ components/app/contact/Contact.jsx
@@ -1,24 +1,52 @@
+import Link from "next/link";
 
 const Contact = () => {
   return (
-    <section className="px-40">
-      <div className="flex items-start justify-between">
+    <section className="px-40 py-20 max-w-screen-xl mx-auto">
+      <h2 className="text-black font-bold font-kameron text-4xl mb-6">
+        Contact Us
+      </h2>
+      <div className="flex items-start gap-40 justify-between">
         <div className="w-full md:w-1/2">
-          <h2 className="text-black font-bold font-kameron text-4xl mb-6">
-            Join us on this journey
-          </h2>
+          <h5>Get in touch</h5>
           <p className="text-secondary font-medium text-[16px] text-justify md:max-w-[52rem]">
-            Whether you're a seasoned <span className="text-primary">TED</span>{" "}
-            enthusiast or a first-time attendee, we invite you to join us on
-            this journey of exploration and inspiration. Together, let's create
-            a world where ideas know no bounds and where each voice has the
-            power to shape a brighter future.
+            Have questions or suggestions? Reach out to us via the contact
+            form,and we'll be happy to connect.
           </p>
+          <Link href={"tel:08160812613"}>08160812613</Link>
+          <Link href={"mailto:tedxfuoye@gmail.com"}>tedxfuoye@gmail.com</Link>
         </div>
-        <div className="w-full md:w-1/2">form</div>
+        <div className="w-full md:w-1/2 bg-white rounded-md p-8">
+          <form action="">
+            <label htmlFor="fullname">Full name</label>
+            <input
+              type="text"
+              name="fullname"
+              id="fullname"
+              placeholder="John Doe"
+            />
+            <label htmlFor="email">Email address</label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              placeholder="example@gmail.com"
+            />
+            <label htmlFor="message">Your message</label>
+            <textarea
+              name="message"
+              id="message"
+              placeholder="Your message"
+            ></textarea>
+
+            <button className="bg-primary hover:bg-primary/90 delay-75 duration-200 px-6 py-4 text-white text-[1.2rem] rounded-lg mt-10">
+              Send message
+            </button>
+          </form>
+        </div>
       </div>
     </section>
   );
-}
+};
 
-export default Contact
+export default Contact;
